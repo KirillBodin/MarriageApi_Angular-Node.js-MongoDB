28d73ebf81d005ab46930e8531c461175736f8a2
kirill.bodin <49336146+KirillBodin@users.noreply.github.com>
2022-07-28 18:04:12 +0300

    Update keys.dev.js

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGODB_URI: 'mongodb+srv://kere:BFbMq0ryMBERqnxZ@cluster0.jj2vm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  MONGODB_URI: 'mongodb+srv://kere:***********.jj2vm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   SESSION_SECRET: 'some secret value',
   SENDGRID_API_KEY: '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',
   EMAIL_FROM: 'nodejs@email.ru',
